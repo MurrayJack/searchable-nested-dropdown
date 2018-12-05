7e9fd8f45e52a858807f80d2804ba53accca41ad
MurrayJack <murrayjack@gmail.com>
2018-12-05 17:36:57 +1100

    just getting some files there

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SearchableNestedDropdown } from "./snd/snd.jsx";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Searchable Nested Dropdown</h1>
+      <SearchableNestedDropdown />
     </div>
   );
 }
---
+++ src/snd/snd.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as snd from "./snd.styles.jsx";
+
+export const SearchableNestedDropdown = () => {
+  return (
+    <snd.Wrapper>
+      <div>asd</div>
+      <div>def</div>
+    </snd.Wrapper>
+  );
+};
